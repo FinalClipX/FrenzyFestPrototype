038631140e2ba41e3b67bd77b41ce5dc5259cd0c
FinalClipX <finalclipx@gmail.com>
2018-02-05 12:05:07 -0500

    Provided code for timer in Food_Movement

--- Assets/Food_Movement.cs
+++ Assets/Food_Movement.cs
@@ -1,57 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Food_Movement : MonoBehaviour {
 
     float speed = 9.0f;
     Vector3 target = new Vector3(0, 0, -2);
-    int afterOrigin = 0;
-    bool listenForMouse = false;
-
-    
+    //int afterOrigin = 0;
+    bool gameOver;
+    public Text gameOverText;
+    float frenzyTimer;
+    bool inputOver;    
 
 
     // Use this for initialization
     void Start () {
-		
+        gameOver = false;
+        gameOverText.text = "";
+        inputOver = false;
+        //inputTimer = 1.2f;
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+        float step = speed * Time.deltaTime;
+
         if (transform.position.x == 0.0)
         {
             target = new Vector3(-10, 0, -2);
-            afterOrigin = 1;
-            StartCoroutine(WaitASec());
-        }
+            WaitForInput();
 
-        if (afterOrigin != 1)
+        }
+        else
         {
-            float step = speed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, target, step);
         }
-
     }
 
-    public IEnumerator WaitASec()
+    IEnumerator WaitForInput()
     {
-        listenForMouse = true;
-
-        
-
-        yield return new WaitForSeconds(1.2f);
-        float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, target, step);
-    }
-
-    private void OnMouseDown()
-    {
-        if(listenForMouse)
+        while(true)
         {
-
+            Debug.Log("Test");
+            yield return new WaitForSeconds(1.2f);
+            if(Input.GetMouseButtonDown(0))
+            {
+                gameOver = true;
+            }
+            if (gameOver)
+            {
+                gameOverText.text = "You Win!";
+            }
+            yield return null;
         }
     }
-
 }
